f5874b07947396d08e2279926ec81f9bfba51855
leeheeae <chromeee@daum.net>
2021-12-14 12:07:47 +0900

    value 있고 없고 설정

--- js/main.js
+++ js/main.js
@@ -1,5 +1,35 @@
+//value가 있을 경우  active한 상태
 const inputCheck = () => {
+    const inputs = document.querySelectorAll('.inputForm input');
 
+    for (item of inputs) {
+        item.addEventListener('input', (e) => {
+			inputChangeCheckItem(e.target);
+        });
+        item.addEventListener('focus', (e) => {
+            
+        })
+    }
+    
 }
+
+// value가 있고 없고 설정
+const inputChangeCheckItem = (item) => {
+    if (item.value.length > 0) {
+        item.classList.add('focus');
+    } else {
+        item.classList.remove('focus');
+    }
+}
+
+//focus 됐을 때 value가 있으면 x표시 나타나도록
+const inputFocusValue = (item) => {
+    if (item.value.length > 0) {
+        item.classList.add('focus');
+    } else {
+        item.classList.remove('focus');
+    }
+}
+
 
 window.addEventListener('load', inputCheck)
